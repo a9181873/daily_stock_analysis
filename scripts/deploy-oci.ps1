b3447605e3f7bb4aab0f3a2882cddae7868ab9bb
a9181873 <a9181873@gmail.com>
2026-05-02 16:36:15 +0800

    [修复] 指定 OCI 部署使用的 SSH 密鑰路徑

--- scripts/deploy-oci.ps1
+++ scripts/deploy-oci.ps1
@@ -6,7 +6,7 @@ $REMOTE_PATH = "~/daily_stock_analysis"
 Write-Host "Starting deployment to OCI ($IP)..." -ForegroundColor Cyan
 $remoteCmd = "cd $REMOTE_PATH && git pull origin main && docker-compose -f ./docker/docker-compose.yml up -d --build"
 
-ssh -o ConnectTimeout=10 "${USER}@${IP}" "$remoteCmd"
+ssh -i "$HOME\.ssh\new_oci.key" -o ConnectTimeout=10 "${USER}@${IP}" "$remoteCmd"
 
 if ($LASTEXITCODE -eq 0) {
     Write-Host "Deployment Successful! Visit http://stock168.dky.tw:8000" -ForegroundColor Green
